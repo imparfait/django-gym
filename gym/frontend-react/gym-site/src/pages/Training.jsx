d0040abdbe96536f0b74addf8bbabff067d34369
Sofiia <sonyprh13.05@gmail.com>
2025-02-14 12:31:15 +0100

    fetched workout classes from backend

--- gym/frontend-react/gym-site/src/pages/Training.jsx
+++ gym/frontend-react/gym-site/src/pages/Training.jsx
@@ -1,10 +1,33 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function Training() {
-    return(
-      <div className="training-page">
-        <h1>Training timetable</h1>
-      </div>
-    )
-  }
-  
-  export default Training;
+  const [trainings, setTrainings] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8000/workout/classes/")
+      .then(response => {
+        setTrainings(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching training data:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Training Schedule</h1>
+      <ul>
+        {trainings.map(training => (
+          <li key={training.id}>
+            {training.name} - {training.start_time} to {training.end_time} on {training.day}, {training.instructor}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Training;
+
   
